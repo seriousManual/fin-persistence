c4b40a63c0795dff197f2de50999aa1fcc9954f8
zaphod1984 <zaphod84@gmx.de>
2014-05-23 17:21:32 +0200

    refrain from only using a streaming interface

--- tests/Persistence.tests.js
+++ tests/Persistence.tests.js
@@ -8,8 +8,24 @@ function createConnectionMock() {
 
     return {
         log: log,
-        query: function(query, parameters) {
+        query: function(query, parameters, callback) {
             log.push([query, parameters]);
+
+            process.nextTick(function() {
+                callback(null, [
+                    {
+                        id: 'fooId',
+                        mandant_id: 'fooMandant',
+                        purpose: 'fooPurpose',
+                        classification: 'fooClassi',
+                        partner: 'fooPartner',
+                        partnerAccountNumber: 'fooPartnerAcountNr',
+                        partnerBank: 'fooBank',
+                        amount: 1,
+                        date: '2014-01-01 10:10:10'
+                    }
+                ]);
+            });
 
             return {
                 stream: function() {
@@ -25,16 +41,26 @@ function createConnectionMock() {
 }
 
 describe('persistence', function() {
-    it('should', function() {
+    it('should', function(done) {
         var connectionMock = createConnectionMock();
         var myPersistence = new Persistence(connectionMock);
 
-        var outputStream = myPersistence.loadLatestPosition(1);
+        myPersistence.loadLatestPosition(1, function(error, result) {
+            expect(result.mandant()).to.equal('fooMandant');
+            expect(result.date().format('YYYY-MM-DD')).to.equal('2014-01-01');
+            expect(result.purpose()).to.equal('fooPurpose');
+            expect(result.classification()).to.equal('fooClassi');
+            expect(result.partner()).to.equal('fooPartner');
+            expect(result.partnerAccountNumber()).to.equal('fooPartnerAcountNr');
+            expect(result.partnerBank()).to.equal('fooBank');
+            expect(result.amount()).to.equal(1);
 
-        expect(outputStream).to.be.instanceof(OutputStream);
-        expect(connectionMock.log).to.deep.equal([[
-            'SELECT * FROM positions WHERE (mandant_id = ?) ORDER BY id DESC LIMIT 1',
-            [1]
-        ]]);
+            expect(connectionMock.log).to.deep.equal([[
+                'SELECT * FROM positions WHERE (mandant_id = ?) ORDER BY id DESC LIMIT 1',
+                [1]
+            ]]);
+
+            done();
+        });
     });
 });
